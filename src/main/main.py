561839bd712ab5378ce2eb596d7959332d7c0c17
fclesio <flavioclesio@yahoo.com>
2022-06-09 12:24:14 +0200

    API initial file

--- src/main/main.py
+++ src/main/main.py
@@ -1,162 +1,65 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-
-sys.path.append("./")
-
-import logging
-import shutil
-import os
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import plot_confusion_matrix
-from joblib import dump, load
+from typing import Optional
+from fastapi import FastAPI, Request, Body
+from data_models import Loans
+from src.logger.logger_factory import LoggerFactory
 
 
-def setup_custom_logger(name):
-    formatter = logging.Formatter(
-        fmt="%(asctime)s - %(levelname)s - %(module)s - %(message)s"
+logger = LoggerFactory.get_logger(log_level="info")
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def pong():
+    return {"ping": "OK"}
+
+"""
+
+@app.post("/decision/v1")
+async def get_body(request: Request):
+    result = await request.json()
+
+    order_data = Orders(
+        order_id=result["order_id"],
+        customer_id=result["customer_id"],
+        timestamp=result["timestamp"],
+        sku_code=result["sku_code"],
+        zip_code=result["zip_code"],
     )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    return logger
 
+    order_id = order_data.order_id
+    customer_id = order_data.customer_id
 
-logger = setup_custom_logger("root")
-logger.info("[DATA-CLEANUP] - Start data folder deletion")
+    features_calculated = features_processed[
+        (features_processed["order_id"] == order_id)
+        & (features_processed["customer_id"] == customer_id)
+    ].head(
+        1
+    )  # TODO: Check with the Wayfair team cases about duplications in data
 
-logger.info("[DATA-CLEANUP] - Data folder deletion finished")
-logger.info("[DATA-EXTRACTION] - Start data extraction")
-url = "https://raw.githubusercontent.com/fclesio/learning-space/master/Datasets/02%20-%20Classification/default_credit_card.csv"
+    order_hour_of_day: int = features_calculated["order_hour_of_day"].values
 
-logger.info(f"[DATA-EXTRACTION] - Data URL: {url}")
-seed = 42
+    inventory: int = features_calculated["inventory"].values
 
-logger.info(f"[DATA-EXTRACTION] - Random seed for data split: {seed}")
-dict_data_path = {
-    "X_train": "data/train_features.csv",
-    "X_test": "data/test_features.csv",
-    "y_train": "data/train_labels.csv",
-    "y_test": "data/test_labels.csv",
-}
+    payment_status: str = features_calculated["payment_status"].values
 
+    zip_code_available: bool = features_calculated["zip_code_available"].values
 
-def get_raw_from_github(url):
-    df = pd.read_csv(url)
-    return df
+    features = Features(
+        order_hour_of_day, inventory, payment_status, zip_code_available
+    )
 
+    recommendation = predict(model_v1, features)
 
-def get_y(df):
-    y = df.iloc[:, -1:]
-    return y
+    result_dict = {}
+    result_dict["order_id"] = order_id
+    result_dict["recommendation"] = str(recommendation.value).upper()
 
+    logger.info(f"API Input: {result} - Prediction Output: {result_dict}")
 
-def get_X(df):
-    del df["DEFAULT"]
-    del df["ID"]
-    del df["SEX"]
-    del df["PAY_0"]
-    del df["PAY_2"]
-    del df["PAY_3"]
-    del df["PAY_4"]
-    del df["PAY_5"]
-    del df["PAY_6"]
-    del df["BILL_AMT3"]
-    del df["BILL_AMT4"]
-    del df["BILL_AMT5"]
-    del df["BILL_AMT6"]
-    del df["PAY_AMT3"]
-    del df["PAY_AMT4"]
-    del df["PAY_AMT5"]
-    del df["PAY_AMT6"]
-    X = df
-    return X
-
-
-def delete_files(filepath):
-    if os.path.exists(filepath):
-        os.remove(filepath)
-
-
-logger.info(f"[DATA-EXTRACTION] - Get data from Github")
-df = get_raw_from_github(url=url)
-
-logger.info(
-    f"[DATA-EXTRACTION] - Dataframe with {df.shape[0]} rows and {df.shape[1]} columns"
-)
-
-y = get_y(df)
-X = get_X(df)
-
-test_size = 0.10
-logger.info(
-    f"[DATA-EXTRACTION] - Split train and test sets. Test size with {test_size}%"
-)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, random_state=seed, test_size=test_size
-)
-
-logger.info(
-    f"[DATA-EXTRACTION] - Training set with {X_train.shape[0]} samples - Test set with {X_test.shape[0]} samples"
-)
-
-logger.info("[DATA-EXTRACTION] - Create folder")
-if not os.path.isdir("data"):
-    os.mkdir("data")
-for key, value in dict_data_path.items():
-    delete_files(filepath=value)
-
-logger.info("[DATA-EXTRACTION]- Saving files in folder")
-
-np.savetxt(dict_data_path["X_train"], X_train)
-np.savetxt(dict_data_path["X_test"], X_test)
-np.savetxt(dict_data_path["y_train"], y_train)
-np.savetxt(dict_data_path["y_test"], y_test)
-
-logger.info("[DATA-EXTRACTION]- Data extraction finished")
-
-dict_cml_objects_path = {
-    "confusion_matrix": "confusion_matrix.png",
-    "metrics": "meta/metrics.txt",
-}
-
-dict_data_path = {
-    "X_train": "data/train_features.csv",
-    "X_test": "data/test_features.csv",
-    "y_train": "data/train_labels.csv",
-    "y_test": "data/test_labels.csv",
-}
-
-
-logger.info("[TRAINING] - Load training and test data")
-X_train = np.genfromtxt(dict_data_path["X_train"])
-y_train = np.genfromtxt(dict_data_path["y_train"])
-X_test = np.genfromtxt(dict_data_path["X_test"])
-y_test = np.genfromtxt(dict_data_path["y_test"])
-
-logger.info("[TRAINING] - Algo training")
-clf = RandomForestClassifier(max_depth=50, verbose=1)
-clf.fit(X_train, y_train)
-
-dump(clf, "models/model.joblib")
-
-acc = clf.score(X_test, y_test)
-acc = (round(acc, 2)) * 100
-
-logger.info(f"[TRAINING] - Model accuracy: {acc}")
-
-with open(dict_cml_objects_path["metrics"], "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
-
-disp = plot_confusion_matrix(clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues)
-
-plt.savefig(dict_cml_objects_path["confusion_matrix"])
-logger.info("[TRAINING] - Training finished")
+    return result_dict
+"""
