283bdc09c163980a40f6daa1275cc616d52b57cb
fclesio <flavioclesio@yahoo.com>
2022-06-10 15:40:34 +0200

    Streamlit working version with sliders

--- src/main/frontend/streamlit/main.py
+++ src/main/frontend/streamlit/main.py
@@ -1,10 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+sys.path.append(".")
+
 import streamlit as st
 import pandas as pd
 import numpy as np
+#from src.main.prediction.model_predicton import get_default_probability
 
-st.title('Uber pickups in NYC')
 
-st.subheader('Number of pickups by hour')
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+st.title("Layman Brothers Loan")
+st.subheader("Personal Information")
+
+st.subheader("Education")
+st.caption("1: No education - 2: College - 3: Graduation - 4: Masters")
+education = st.slider("Education", 1, 4, 1)
+st.write("Education: ", education)
+
+st.subheader("Marriage")
+st.caption("1: Married - 2: Not Married")
+marriage = st.slider("marriage", 1, 2, 1)
+st.write("Marriage: ", marriage)
+
+st.subheader("Age")
+age = st.slider("age", 18, 120, 18)
+st.write("Age: ", age)
+
+
+st.subheader("Financial Information")
+
+limit_bal = st.slider("Balance Limit", 0, 500000, 0)
+st.write("Balance Limit: ", limit_bal)
+
+bill_amt1 = st.slider("Bill Amount - Installment #1", 0, 500000, 0)
+st.write("Bill Amount - Installment #1: ", bill_amt1)
+
+bill_amt2 = st.slider("Bill Amount - Installment #2", 0, 500000, 0)
+st.write("Bill Amount - Installment #2: ", bill_amt2)
+
+pay_amt1 = st.slider("Payment Amount - Installment #1", 0, 500000, 0)
+st.write("Payment Amount - Installment #1: ", pay_amt1)
+
+pay_amt2 = st.slider("Payment Amount - Installment #2", 0, 500000, 0)
+st.write("Payment Amount - Installment #1: ", pay_amt2)
+
+
+"""
+record = [
+    [limit_bal, education, marriage, age, bill_amt1, bill_amt2, pay_amt1, pay_amt2]
+]
+
+result = get_default_probability(record=record)
+
+st.metric(label="Default Probability", value=f"{result}")
+"""
