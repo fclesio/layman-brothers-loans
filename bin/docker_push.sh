4f1d4698a3e85129531851339c33a57f71c8d7df
fclesio <flavioclesio@yahoo.com>
2023-02-28 14:37:37 +0100

    fix: Set right version for docker push

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -eux pipefail
 
-VERSION="latest"
+API_VERSION="0.0.1"
+DOCKER_REPO="mlopsde"
+APP_NAME="layman-brothers-loans"
 
-docker build -t mlopsde/layman-brothers-loans:$VERSION . &&\
-docker push mlopsde/layman-brothers-loans:$VERSION
+
+docker image build --tag $DOCKER_REPO/$APP_NAME:latest --shm-size=8g . && \
+docker image push $DOCKER_REPO/$APP_NAME:latest
+
+
+docker image build --tag $DOCKER_REPO/$APP_NAME:$API_VERSION --shm-size=8g . && \
+docker image push $DOCKER_REPO/$APP_NAME:$API_VERSION
