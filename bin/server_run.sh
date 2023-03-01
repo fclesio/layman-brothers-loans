0ec93ed2963627e2df879234cc78dfd12597eda7
fclesio <flavioclesio@yahoo.com>
2023-03-01 15:56:25 +0100

    chore: Remove training from startup

--- bin/server_run.sh
+++ bin/server_run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -eux pipefail
 
-python -m pytest src/tests/unit_test/test_requirements.py -o log_cli=true --log-cli-level=INFO \
-
 chmod 777 ./bin/start_api.sh &&
 
 chmod 777 ./bin/start_jupyter.sh &&
 
 chmod 777 ./bin/start_frontend.sh &&
-
-chmod 777 ./bin/start_training.sh &&
-
-sh ./bin/start_training.sh &&
 
 parallel -u ::: './bin/start_api.sh' './bin/start_jupyter.sh' './bin/start_frontend.sh' 
